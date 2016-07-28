bcece646379105200f221c6ad890737a772e71f9
root <root@web00.qc.wanduoduo.com>
2016-07-28 14:15:04 +0800

    docker dm mount other dir

---
+++ docker_dm_mount.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#author: b0lu
+#mail: b0lu_xyz@163.com
+set -e
+function usage(){
+    echo '-----------BY B0LU-------------'
+    echo 'Usage: bash '$0' container_name'
+}
+if [ $# -ne 1 ];then
+    usage
+fi
+
+CONTAINER_NAME=$1
+DOCKER_PROCESS=`ps aux|grep -i 'docker daemon'`
+if [ `echo $DOCKER_PROCESS|grep graph|wc -l` -eq 1 ]; then
+    DOCKER_PATH=`echo $DOCKER_PROCESS|python -c "import sys,re;print re.findall(r\"--graph='(.*)'\",sys.stdin.read().strip())[0]"`
+else
+    DOCKER_PATH='/var/lib/docker'
+fi
+echo $DOCKER_PATH
+METADATA_PATH=$DOCKER_PATH"/devicemapper/metadata/"
+echo $METADATA_PATH
+CONTAINERID=`docker ps -a |grep $CONTAINER_NAME|awk '{print $1}'`
+CONFIG_FILE=$METADATA_PATH`ls $METADATA_PATH|grep $CONTAINERID|grep -v init`
+echo $CONFIG_FILE
+DEVICE_ID=`cat $CONFIG_FILE|python -c "import sys,json;config_data = sys.stdin.read().strip();print json.loads(config_data)['device_id']"`
+echo $DEVICE_ID
+
+DM_NAME=`dmsetup ls|sed -n '1p'|awk '{print $1}'`
+DM_CONFIG_OLD=`dmsetup table $DM_NAME`
+DM_CONFIG=`echo ${DM_CONFIG_OLD% *}`" $DEVICE_ID"
+echo $DM_CONFIG
+
+MOUNT_DIR='/tmp/'`python -c "import random;print random.random().__str__()[2:]"`
+DM_CREATE_NAME=`python -c "import random;print random.random().__str__()[2:]"`
+dmsetup create $DM_CREATE_NAME --table "$DM_CONFIG"
+if [ ! -d $MOUNT_DIR ]; then
+    mkdir -p $MOUNT_DIR
+fi
+mount /dev/mapper/$DM_CREATE_NAME $MOUNT_DIR
+ls $MOUNT_DIR
+echo "挂载目录: "$MOUNT_DIR
