218137135a32fc940a0debaa62dc6ce4f10d21c0
root <root@web00.qc.wanduoduo.com>
2016-07-29 13:49:23 +0800

    only shell

--- docker_dm_mount.sh
+++ docker_dm_mount.sh
@@ -5,15 +5,21 @@ set -e
 function usage(){
     echo '-----------BY B0LU-------------'
     echo 'Usage: bash '$0' container_name'
+    echo '       bash '$0' -d dm_id'
 }
-if [ $# -ne 1 ];then
+if [ $# -lt 1 ];then
     usage
+elif [ $# -eq 2 -a $1 == '-d' ];then
+    REMOVE_ID=$3
+    umount /tmp/$REMOVE_ID
+    dmsetup remove /dev/mapper/$REMOVE_ID
+    echo "删除挂载成功"
+    exit
 fi
-
 CONTAINER_NAME=$1
 DOCKER_PROCESS=`ps aux|grep -i '/usr/bin/docker'`
 if [ `echo $DOCKER_PROCESS|grep graph|wc -l` -eq 1 ]; then
-    DOCKER_PATH=`echo $DOCKER_PROCESS|python -c "import sys,re;print re.findall(r\"--graph='(.*)'\",sys.stdin.read().strip())[0]"`
+    DOCKER_PATH=`echo $DOCKER_PROCESS|sed "s/.*--graph='\(.*\)'.*/\1/"`
 else
     DOCKER_PATH='/var/lib/docker'
 fi
@@ -23,7 +29,7 @@ echo $METADATA_PATH
 CONTAINERID=`docker ps -a |grep $CONTAINER_NAME|awk '{print $1}'`
 CONFIG_FILE=$METADATA_PATH`ls $METADATA_PATH|grep $CONTAINERID|grep -v init`
 echo $CONFIG_FILE
-DEVICE_ID=`cat $CONFIG_FILE|python -c "import sys,json;config_data = sys.stdin.read().strip();print json.loads(config_data)['device_id']"`
+DEVICE_ID=`cat $CONFIG_FILE|sed -r 's/.*device_id":([^,]*),.*/\1/g'`
 echo $DEVICE_ID
 
 DM_NAME=`dmsetup ls|grep -v skip_block_zeroing|sed -n '1p'|awk '{print $1}'`
@@ -32,7 +38,8 @@ DM_CONFIG=`echo ${DM_CONFIG_OLD% *}`" $DEVICE_ID"
 echo $DM_CONFIG
 
 MOUNT_DIR=`mktemp -d`
-DM_CREATE_NAME=`echo ${MOUNT_DIR##*/}`
+DM_CREATE_NAME=${MOUNT_DIR##*/}
+echo $DM_CREATE_NAME
 dmsetup create $DM_CREATE_NAME --table "$DM_CONFIG"
 if [ ! -d $MOUNT_DIR ]; then
     mkdir -p $MOUNT_DIR
